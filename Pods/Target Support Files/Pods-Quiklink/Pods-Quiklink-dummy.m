ef7bd2b3b0ac2ac5f28048d69e5f7a2cbe9fb938
Matt Bommer <matthew.bommer@willowtreeapps.com>
2023-02-08 15:28:15 -0500

    Update podfile and pods

---
+++ Pods/Target Support Files/Pods-Quiklink/Pods-Quiklink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Quiklink : NSObject
+@end
+@implementation PodsDummy_Pods_Quiklink
+@end
--- Pods/Target Support Files/Pods-Quiklink/Pods-Quiklink-umbrella.h
+++ Pods/Target Support Files/Pods-Quiklink/Pods-Quiklink-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_RssReaderVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RssReaderVersionString[];
+FOUNDATION_EXPORT double Pods_QuiklinkVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QuiklinkVersionString[];
 
